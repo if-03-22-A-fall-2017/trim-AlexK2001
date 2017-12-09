d926060e21f03244a1f7754bd1427b78eeede48c
AlexK2001 <31956992+AlexK2001@users.noreply.github.com>
2017-12-09 22:45:52 +0100

    finished

--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,42 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+#include "trim.h"
+#include <string.h>
+#include "shortcut.h"
+
+void  trim(const char *source, char *trimmed_string)
+{
+  trimmed_string = fill_trimmed_string(source, trimmed_string);
+}
+char* fill_trimmed_string(const char *source, char *trimmed_string)
+{
+    int insertPosition = 0;
+    int max_len = ending_position(source);
+    int readPosition = blanks_at_the_beginning(source);
+      for (; readPosition < max_len; readPosition++)
+      {
+          trimmed_string[insertPosition] = source[readPosition];
+          insertPosition++;
+      }
+      trimmed_string[insertPosition] = '\0';
+      return trimmed_string;
+}
+int blanks_at_the_beginning(const char *source)
+{
+  int readPosition = 0;
+  while(source[readPosition]== ' ')
+  {
+    readPosition++;
+  }
+  return readPosition;
+}
+int ending_position(const char *source)
+{
+  int max_len = strlen(source);
+  while(source[max_len-1] == ' ')
+  {
+    max_len--;
+  }
+  return max_len;
+}
--- trim.h
+++ trim.h
@@ -10,3 +10,14 @@
  * Interface for trim
  * ----------------------------------------------------------
  */
+#include <string.h>
+#ifndef ___TRIM_H
+#define ___TRIM_H
+#define STRLEN 100
+ void trim 	( 	const char *  	source,
+    char *  	trimmed_string);
+
+     char* fill_trimmed_string(const char *source, char *trimmed_string);
+     int ending_position(const char *source);
+    int blanks_at_the_beginning(const char *source);
+    #endif
